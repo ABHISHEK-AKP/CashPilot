38ae969ac3ccf29a040234f6fd8dc624b99d608e
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-03-23 21:44:20 -0400

    Login with google

--- app/src/main/java/com/example/project/LogIn.java
+++ app/src/main/java/com/example/project/LogIn.java
@@ -5,30 +5,50 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LogIn extends AppCompatActivity {
     TextInputEditText editTextEmail, editTextPassword;
     Button btnLogIn;
+    Button googleSignInBtn;
     ProgressBar progressBar;
     TextView signUpNow;
     FirebaseAuth mAuth;
+    GoogleSignInClient googleSignInClient;
     @Override
     public void onStart() {
         super.onStart();
@@ -42,6 +62,7 @@ public class LogIn extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseApp.initializeApp(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
@@ -51,6 +72,19 @@ public class LogIn extends AppCompatActivity {
         btnLogIn = findViewById(R.id.btn_login);
         progressBar = findViewById(R.id.progressBar);
         signUpNow = findViewById(R.id.signUpNow);
+        googleSignInBtn = findViewById(R.id.btn_google_login);
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                        .requestEmail()
+                                .build();
+        googleSignInClient = GoogleSignIn.getClient(LogIn.this,options);
+        googleSignInBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent googleSignInIntent = googleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(googleSignInIntent);
+            }
+        });
         signUpNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,7 +96,6 @@ public class LogIn extends AppCompatActivity {
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
                 String email,password;
                 email =String.valueOf(editTextEmail.getText());
                 password =String.valueOf(editTextPassword.getText());
@@ -74,6 +107,7 @@ public class LogIn extends AppCompatActivity {
                     Toast.makeText(LogIn.this,"Enter Password",Toast.LENGTH_SHORT).show();
                     return;
                 }
+                progressBar.setVisibility(View.VISIBLE);
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
@@ -102,4 +136,33 @@ public class LogIn extends AppCompatActivity {
             return insets;
         });
     }
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK){
+                Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                try{
+                    GoogleSignInAccount signInAccount= accountTask.getResult(ApiException.class);
+                    AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(),null);
+                    mAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                mAuth = FirebaseAuth.getInstance();
+//                                Glide.with(LogIn.this).load(mAuth.getCurrentUser().getPhotoUrl()).into(new ImageView());
+                                Toast.makeText(LogIn.this,"Successfully logged in",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(LogIn.this,"Failed Log in"+task.getException(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                catch (ApiException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
+
 }

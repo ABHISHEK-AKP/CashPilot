2b1b1a7cc794fa60ffacf4b9f07c05678ab58835
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-04-01 10:23:40 -0400

    Added Scan Reciepts

--- app/src/main/java/com/example/project/OpenAIService.java
+++ app/src/main/java/com/example/project/OpenAIService.java
@@ -6,8 +6,7 @@ import retrofit2.http.POST;
 
 public interface OpenAIService {
     @Headers({
-            "Content-Type: application/json",
-            "Authorization: Bearer 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"
+            "Content-Type: application/json"
     })
     @POST("v1/chat/completions")
     Call<OpenAIResponse> getChatResponse(@Body OpenAIRequest request);
